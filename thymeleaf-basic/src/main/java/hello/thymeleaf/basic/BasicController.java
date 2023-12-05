563c92e268fc82180c92006133b5899851770492
jipark1106 <jipark1106@gmail.com>
2023-12-05 14:27:29 +0900

    20231205 review

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -80,13 +80,14 @@ public class BasicController {
         return "basic/date";
     }
 
-    @GetMapping("link")
+    @GetMapping("/link")
     public String link(Model model){
         model.addAttribute("param1","data1");
         model.addAttribute("param2","data2");
         return "basic/link";
     }
 
+    @GetMapping("/literal")
     public String literal(Model model) {
         model.addAttribute("data", "Spring!");
         return "basic/literal";
